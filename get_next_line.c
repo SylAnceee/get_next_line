917d2a09d3ecc7f2e4208962f002861a8b274830
SylAnceee <syl.contactpro@gmail.com>
2024-11-06 09:57:22 +0100

    V1

---
+++ get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abreuil <abreuil@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 13:41:38 by abreuil           #+#    #+#             */
+/*   Updated: 2024/10/01 14:47:51 by abreuil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char    *get_next_line(int fd)
+{
+    // A function that will call read till the read pointer reaches a "$" "\n"
+    
+
+    static  char *buffer;
+    read();
+    open();
+    return ("line that has been read or NULL if nothing to read or error occured")
+    
+}
